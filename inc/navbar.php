d05dac662824a2b007670c8aeba94dbea33d8c76
steevenU1 <154490794+steevenU1@users.noreply.github.com>
2024-09-06 15:02:49 -0600

    Initial commit

---
+++ inc/navbar.php
@@ -0,0 +1,93 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="./assets/captura.png" alt="logo bbm">
+
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Usuario
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Nuevo
+          </a>
+          <a class="navbar-item">
+            Lista
+          </a>
+          <a class="navbar-item">
+            Buscar
+          </a>
+          
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Categorias
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Nueva
+          </a>
+          <a class="navbar-item">
+            Lista
+          </a>
+          <a class="navbar-item">
+            Buscar
+          </a>
+          
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Productos
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Nuevo
+          </a>
+          <a class="navbar-item">
+            Lista
+          </a>
+          <a class="navbar-item">
+            Por categoría
+          </a>
+          <a class="navbar-item">
+            Buscar
+          </a>
+          
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary is-rounded">
+            Mi Perfil
+          </a>
+          <a class="button is-light is-rounded">
+            Salir
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php
+    include "./inc/head.php";
+  ?>
+</head>
+<body>
+  <?php
+    include "./inc/navbar.php";
+
+    include "./inc/script.php";
+  ?>
+    
+</body>
+</html>
